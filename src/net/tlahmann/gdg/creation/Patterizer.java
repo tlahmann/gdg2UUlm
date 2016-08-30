ced3b95479439ed18c0bbe59d45c39a3e68a030c
Tobias Lahmann <tobias.lahmann@uni-ulm.de>
2016-08-30 13:05:44 +0200

    Create patterizer bone

---
+++ src/net/tlahmann/gdg/creation/Patterizer.java
@@ -0,0 +1,35 @@
+package net.tlahmann.gdg.creation;
+
+import processing.core.PApplet;
+
+public class Patterizer  extends PApplet {
+
+		private static final int WIDTH = 800;
+		private static final int HEIGHT = 800;
+
+		public void setup() {
+			
+		}
+
+		public void settings() {
+			size(WIDTH, HEIGHT);
+		}
+
+		public void draw() {
+		}
+
+		public void mouseDragged() {
+
+		}
+
+		public void mouseClicked() {
+		}
+
+		public void keyPressed() {
+		}
+
+		public static void main(String[] args) {
+			PApplet.main(new String[] { Patterizer.class.getName() });
+		}
+	
+}
