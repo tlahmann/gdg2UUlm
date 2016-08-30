881d15bf001db9018c54232233367dc4802a9966
Tobias Lahmann <tobias.lahmann@uni-ulm.de>
2016-08-31 01:14:40 +0200

    Add second menu option

--- src/net/tlahmann/gdg/creation/Patterizer.java
+++ src/net/tlahmann/gdg/creation/Patterizer.java
@@ -3,6 +3,7 @@ package net.tlahmann.gdg.creation;
 import java.util.List;
 
 import net.tlahmann.gdg.creation.gui.HScrollbar;
+import net.tlahmann.gdg.creation.helper.Tuple;
 import net.tlahmann.gdg.creation.object.PCircle;
 import net.tlahmann.gdg.creation.object.PStar;
 
@@ -16,9 +17,9 @@ public class Patterizer extends PApplet {
 	private static final int WIDTH = 1024;
 	private static final int HEIGHT = 720;
 
-	public PFont f14, f24;
-	private int maxNumberOfArms;
-	public int numberOfArms;
+	public PFont f14, f20;
+	public Tuple<Integer, Integer> numberOfArms;
+	public Tuple<Integer, Integer> thickness;
 	float guiTimeout = 0;
 
 	List<PStar> stars;
@@ -26,10 +27,11 @@ public class Patterizer extends PApplet {
 
 	public void setup() {
 		// Create the font
-		f24 = createFont("./src/net/tlahmann/gdg/data/OpenSans-Regular.ttf", 24);
+		f20 = createFont("./src/net/tlahmann/gdg/data/OpenSans-Regular.ttf", 20);
 		f14 = createFont("./src/net/tlahmann/gdg/data/OpenSans-Regular.ttf", 14);
 
-		maxNumberOfArms = 12;
+		numberOfArms = new Tuple<Integer, Integer>(0, 12);
+		thickness = new Tuple<Integer, Integer>(0, 12);
 
 		// noLoop();
 		frameRate(60);
@@ -37,10 +39,13 @@ public class Patterizer extends PApplet {
 		init();
 	}
 
-	HScrollbar hs1;
+	HScrollbar hs1, hs2;
 
 	private void init() {
-		numberOfArms = round(map(hs1.getPos(), 0, hs1.sposMax, 0, maxNumberOfArms - 1));
+		thickness.x = round(map(hs1.getPos(), 0, hs1.sposMax, 0, thickness.y - 1));
+		hs1.text = thickness.x.toString();
+		numberOfArms.x = round(map(hs2.getPos(), 0, hs2.sposMax, 0, numberOfArms.y - 1));
+		hs2.text = numberOfArms.x.toString();
 
 		stars = new ArrayList<PStar>();
 		circles = new ArrayList<PCircle>();
@@ -48,7 +53,7 @@ public class Patterizer extends PApplet {
 		for (int j = 2; j < height + 100; j += 74) {
 			int k = (evodd = !evodd) ? 42 : 0;
 			for (int i = 0; i < width + 100; i += 84) {
-				stars.add(new PStar(this, new float[] { i + k, j }, 50, numberOfArms));
+				stars.add(new PStar(this, new float[] { i + k, j }, 50, numberOfArms.x));
 				// circles.add(new PCircle(this, new float[] { i + k, j }, 14 +
 				// j * (5.0f / 80.0f)));
 			}
@@ -56,7 +61,8 @@ public class Patterizer extends PApplet {
 	}
 
 	public void initGui() {
-		hs1 = new HScrollbar(this, 35, 60, 200, 12, 1);
+		hs1 = new HScrollbar(this, 20, 60, 200, 12, 1);
+		hs2 = new HScrollbar(this, 20, 110, 200, 12, 1);
 	}
 
 	public void settings() {
@@ -91,16 +97,22 @@ public class Patterizer extends PApplet {
 		// gui elements
 		strokeWeight(1);
 		fill(255);
-		textFont(f24);
-
+		
 		textAlign(LEFT);
-		text("Arms", 20, 44);
-		rect(20, 60 - 12 / 2, 12, 12);
-		rect(203, 60 - 12 / 2, 12, 12);
 
+		textFont(f20);
+		text("Thickness", 20, 44);
 		hs1.update();
 		hs1.display();
-		if (numberOfArms != round(map(hs1.getPos(), 0, hs1.sposMax, 0, maxNumberOfArms - 1))) {
+		if (thickness.x != round(map(hs1.getPos(), 0, hs1.sposMax, 0, thickness.y - 1))) {
+			init();
+		}
+
+		textFont(f20);
+		text("Arms", 20, 94);
+		hs2.update();
+		hs2.display();
+		if (numberOfArms.x != round(map(hs2.getPos(), 0, hs2.sposMax, 0, numberOfArms.y - 1))) {
 			init();
 		}
 	}
