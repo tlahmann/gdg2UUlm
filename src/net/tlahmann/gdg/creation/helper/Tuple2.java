49f9a47a5751992038712a4ec80768a1048dca4a
Tobias Lahmann <tobias.lahmann@uni-ulm.de>
2016-09-05 14:47:46 +0200

    Rename tuple

--- src/net/tlahmann/gdg/creation/helper/Tuple2.java
+++ src/net/tlahmann/gdg/creation/helper/Tuple2.java
@@ -1,10 +1,10 @@
 package net.tlahmann.gdg.creation.helper;
 
-public class Tuple<X, Y> {
+public class Tuple2<X, Y> {
 	public X x;
 	public Y y;
 
-	public Tuple(X x, Y y) {
+	public Tuple2(X x, Y y) {
 		this.x = x;
 		this.y = y;
 	}
