09c91265eab308e361e6275683675568023c9961
Tobias Lahmann <tobias.lahmann@uni-ulm.de>
2016-09-05 14:47:56 +0200

    Create tuple3

---
+++ src/net/tlahmann/gdg/creation/helper/Tuple3.java
@@ -0,0 +1,13 @@
+package net.tlahmann.gdg.creation.helper;
+
+public class Tuple3<X, Y, Z> {
+	public X x;
+	public Y y;
+	public Z z;
+
+	public Tuple3(X x, Y y, Z z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
