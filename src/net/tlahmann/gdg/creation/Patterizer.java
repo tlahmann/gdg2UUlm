9c22941249af6627c0cc0d8fdb4ac7ed54b2b01f
Tobias Lahmann <tobias.lahmann@uni-ulm.de>
2016-09-06 13:42:45 +0200

    Remove circle and line class
    since it is now drawn by the PStar

--- src/net/tlahmann/gdg/creation/Patterizer.java
+++ src/net/tlahmann/gdg/creation/Patterizer.java
@@ -2,7 +2,6 @@ package net.tlahmann.gdg.creation;
 
 import java.util.List;
 
-import net.tlahmann.gdg.creation.object.PCircle;
 import net.tlahmann.gdg.creation.object.PStar;
 
 import java.util.ArrayList;
@@ -16,7 +15,6 @@ public class Patterizer extends PApplet {
 	private static final int HEIGHT = 720;
 
 	List<PStar> stars;
-	List<PCircle> circles;
 
 	public Gui gui;
 
@@ -30,14 +28,11 @@ public class Patterizer extends PApplet {
 
 	private void init() {
 		stars = new ArrayList<PStar>();
-		circles = new ArrayList<PCircle>();
 		boolean evodd = true;
 		for (int j = (int) gui.originY.y; j < height + 100; j += gui.distanceY.y) {
 			float k = (evodd = !evodd) ? gui.offset : 0;
 			for (int i = (int) gui.originX.y; i < width + 100; i += gui.distanceX.y) {
 				stars.add(new PStar(this, new float[] { i + k, j }, gui.radius.y, gui.rotation.y, gui.elements.y));
-				// circles.add(new PCircle(this, new float[] { i + k, j }, 14 +
-				// j * (5.0f / 80.0f)));
 			}
 		}
 		gui.changes = false;
@@ -59,8 +54,6 @@ public class Patterizer extends PApplet {
 
 		for (PStar s : stars)
 			s.display();
-		for (PCircle c : circles)
-			c.run();
 	}
 
 	void screenshot() {
