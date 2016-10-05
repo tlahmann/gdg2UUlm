5019b53dc33fe4ac60867d6655f98a02a25fdac1
Tobias Lahmann <tobias.lahmann@uni-ulm.de>
2016-10-05 17:59:37 +0200

    Write javadoc comments

--- src/net/tlahmann/gdg/creation/object/PShape.java
+++ src/net/tlahmann/gdg/creation/object/PShape.java
@@ -4,11 +4,41 @@ import net.tlahmann.gdg.creation.Patterizer;
 import processing.core.PApplet;
 import processing.core.PVector;
 
+/**
+ * <h1>PShape</h1>
+ * <p>
+ * The <b>PShape</b> class represents an elements. Element parameters can be
+ * modified within the <b>gui</b> class.
+ * </p>
+ * 
+ * @author Tobias Lahmann
+ * @version 1.0
+ * @since 2016-10-05
+ * 
+ * @see net.tlahmann.gdg.creation.Gui
+ */
 public class PShape {
+	/*
+	 * private variables not to be modified
+	 */
 	private Patterizer parent;
 	private PVector[] points;
 	private PVector center;
 
+	/**
+	 * <p>
+	 * Constructor used to create one PShape of a specific form and position.
+	 * </p>
+	 * 
+	 * @since 2016-10-05
+	 * 
+	 * @param p the parent element of type Patterizer. Used to hold the instance
+	 *            to be drawn on
+	 * @param pos the position of the center point
+	 * @param radius the expansion of the PShape
+	 * @param rotation used to rotate around the center
+	 * @param ends number of end points in this PShape
+	 */
 	public PShape(Patterizer p, float[] pos, float radius, float rotation, float ends) {
 		parent = p;
 
@@ -21,6 +51,14 @@ public class PShape {
 		}
 	}
 
+	/**
+	 * <p>
+	 * The display method is used to draw the PShape onto a canvas provided by
+	 * its parent. The parent needs to extend the <b>PApplet</b> class.
+	 * </p>
+	 * 
+	 * @since 2016-10-05
+	 */
 	public void display() {
 		parent.stroke(parent.gui.colors[0]);
 		parent.strokeWeight(parent.gui.thickness.y);
